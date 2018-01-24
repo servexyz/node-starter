b7ee21c355b1d694b205bcbcbd55dfe93087e023
alechp <me@alechp.com>
2018-01-24 14:13:24 -0800

    Initialized

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: Alec Hale-Pletka <alechp>
+ * @Date:   2018-01-24T13:57:15-08:00
+ * @Email:  alec@bubblegum.academy
+ * @Last modified by:   alechp
+ * @Last modified time: 2018-01-24T14:05:46-08:00
+ */
+console.log("hi");
