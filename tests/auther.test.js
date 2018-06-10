306f005196fe35d2c5c9c1fc6fb112f2ac4113b2
alechp <me@alechp.com>
2018-06-10 10:43:42 -0700

    Removed cruft (ie. aws-sls-auther link in package) & fixed output typo

--- tests/auther.test.js
+++
@@ -1,7 +0,0 @@
-import { test } from "ava";
-import { authenticate, isAuthorized } from "aws-sls-auther";
-
-test("Authenticate works", async t => {
-  let token = authenticate("alechp", "123456");
-  t.truthy(token);
-});
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "main.js",
-    output: "commonjs"
+    libraryTarget: "commonjs"
   },
   target: "node",
   externals: [NodeExternals()],
