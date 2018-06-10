f4f1b8865854ef9af1d2365a77c43c95875035dc
alechp <me@alechp.com>
2018-06-10 09:37:13 -0700

    Reverted to using babel (ran into issue with mapping module name when using webpack config options)
    * Added source-map option in webpack (now exports main.js.map in build dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const NodemonPlugin = require("nodemon-webpack-plugin");
+const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
 
 module.exports = {
   entry: ["idempotent-babel-polyfill", path.resolve(__dirname, "src/index.js")],
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "main.bundle.js"
+    filename: "main.js"
   },
+  target: "node",
   module: {
     rules: [
       {
@@ -15,18 +17,27 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@ava/babel-preset-stage-4"],
-            plugins: [
-              ["babel-plugin-dotenv", { replacedModuleName: "babel-dotenv" }]
-            ]
+            babelrc: true
           }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "source-map-loader"
         }
       }
     ]
   },
+  devtool: "source-map",
+  resolve: {
+    symlinks: false
+  },
   plugins: [
     new NodemonPlugin({
       verbose: true
-    })
+    }),
+    new DuplicatePackageCheckerPlugin()
   ]
 };
