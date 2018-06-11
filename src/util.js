3e85eacc3e4da2b80a6cb3b7d21bc0f1a0824da4
alechp <me@alechp.com>
2018-06-11 16:14:31 -0700

    Removed util (which was being used only for testing aws-sls-auther import)

--- src/util.js
+++
@@ -1,6 +0,0 @@
-import { authenticate } from "aws-sls-auther";
-
-export async function auth() {
-  let token = await authenticate("alechp", "123456");
-  console.log(`Token from Ava: ${token}`);
-}
