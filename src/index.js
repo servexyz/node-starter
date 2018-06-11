40d79af882b20298aef655c4aeb405dd61f8eb2b
alechp <me@alechp.com>
2018-06-11 16:31:52 -0700

    Added honesty test (by importing npm-starter-sample-module)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 import { SAMPLE_ENV } from "babel-dotenv";
 console.log(`Hello ${SAMPLE_ENV}!`);
-
-console.log(`\n Index \n ------------- \n`);
--- tests/foo.test.js
+++ tests/foo.test.js
@@ -1,5 +1,6 @@
 import test from "ava";
 import { SAMPLE_ENV } from "babel-dotenv";
+import { honesty } from "npm-starter-sample-module";
 
 test("foo", t => {
   t.pass();
@@ -13,4 +14,8 @@ test("bar", async t => {
 
 test("SAMPLE_ENV loads", t => {
   t.is(SAMPLE_ENV, "foobar");
+});
+
+test("honesty is the best policy", t => {
+  t.truthy(honesty());
 });
