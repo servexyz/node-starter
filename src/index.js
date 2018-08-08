2a9744db6dc05bbe32b9285bd14f75cf808820f4
alechp <me@alechp.com>
2018-08-08 09:53:37 -0700

    Added gif demo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import { SAMPLE_ENV } from "babel-dotenv";
 console.log(`Hello ${SAMPLE_ENV}!`);
+//NOTE: ^ this will fail unless you change ".env.sample" to ".env"
+
+console.log("live reload works");
