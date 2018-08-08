3b5a5abd9c4502b5f19f9ab6e24c38cf89da50e6
alechp <me@alechp.com>
2018-08-08 09:32:51 -0700

    Clarified scripts
    * dev:moduleReload script now
    * removed verbose webpack printing
    * cleaned up test scripts to make more sense
    
    consider moving ava config into separate config file ?

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import { SAMPLE_ENV } from "babel-dotenv";
 console.log(`Hello ${SAMPLE_ENV}!`);
+
+console.log("hi");
+
+console.log("hi");
+console.log("hi3");
+
+console.log("hi4");
--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
   },
   plugins: [
     new NodemonPlugin({
-      verbose: true
+      verbose: false
     }),
     new DuplicatePackageCheckerPlugin()
   ]
