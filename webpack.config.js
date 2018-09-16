f4fba222439d52b57f9f7219561a5391e95efbaf
alechp <me@alechp.com>
2018-09-16 15:46:21 -0700

    Local tests passing.
    Next, publish sample-starter module and test remote

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const NodeExternals = require("webpack-node-externals");
 const SizePlugin = require("size-plugin");
 
 module.exports = {
-  entry: ["idempotent-babel-polyfill", path.resolve(__dirname, "src/index.js")],
+  entry: [path.resolve(__dirname, "src/index.js")],
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "main.js",
